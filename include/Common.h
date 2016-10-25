cce8db0318ea0487251d8c77df2fcfdcd5b57be6
Snehasish Kumar <ska124@sfu.ca>
2016-10-25 02:21:29 -0700

    Fixed Common.h reference to header

--- include/Common.h
+++ include/Common.h
@@ -1,7 +1,7 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include "../lib/InstruMem/InstruMem.h"
+#include "InstruMem.h"
 #include "llvm/ADT/DenseSet.h"
 #include "llvm/ADT/SetVector.h"
 #include "llvm/ADT/StringRef.h"
