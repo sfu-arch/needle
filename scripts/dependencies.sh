8e2833d9b79a83fec6e942acac5082c831620140
Snehasish Kumar <ska124@sfu.ca>
2017-01-28 22:03:01 -0800

    Adding CI support

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sudo apt-get update
-wget http://releases.llvm.org/3.8.1/clang+llvm-3.8.1-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-tar xf clang+llvm-3.8.1-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+#sudo apt-get update
+
