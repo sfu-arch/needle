803fe3aeff912d95a43506a2bb38ea9b2272b869
Snehasish Kumar <ska124@sfu.ca>
2017-02-05 19:09:01 -0800

    Update top level Makefiles to add all the build rules availabele for examples.

--- examples/workloads/Rules.mk
+++ examples/workloads/Rules.mk
@@ -1,73 +1,75 @@
 all: needle-run-path 
 
-setup: .setup.success
-.setup.success: 
+setup: .setup.done
+.setup.done: 
 	@echo SETUP
 	@rm -rf $(FUNCTION)
 	@mkdir $(FUNCTION)
 	$(LLVM_OBJ)/llvm-link $(BC) $(BITCODE_REPO)/$(LLVM_VERSION)/lib/m.bc -o $(FUNCTION)/$(NAME).bc
 	$(LLVM_OBJ)/opt -O2 -disable-loop-vectorization -disable-slp-vectorization  $(FUNCTION)/$(NAME).bc -o $(FUNCTION)/$(NAME).bc
-	@touch .setup.success
+	@touch .setup.done
 
-epp-inst: .setup.success .epp-inst.success
-.epp-inst.success: .setup.success 
+epp-inst: .setup.done .epp-inst.done
+.epp-inst.done: .setup.done 
 	@echo EPP-INST
 	cd $(FUNCTION) && \
 	export PATH=$(LLVM_OBJ):$(PATH) && \
 		$(NEEDLE_OBJ)/epp $(LDFLAGS) -L$(NEEDLE_LIB) -epp-fn=$(FUNCTION) $(NAME).bc -o $(NAME)-epp $(LIBS) 2> ../epp-inst.log
-	@touch .epp-inst.success
+	@touch .epp-inst.done
 
-prerun: .setup.success 
-.prerun.success: .setup.success 
+prerun: .setup.done 
+.prerun.done: .setup.done 
 ifdef PRERUN
 	cd $(FUNCTION) && \
 	bash -c $(PRERUN)
 endif
-	@touch .prerun.success
+	@touch .prerun.done
 
-epp-run: .epp-inst.success .epp-run.success .prerun.success
-.epp-run.success: .epp-inst.success .prerun.success
+epp-run: .epp-inst.done .epp-run.done .prerun.done
+.epp-run.done: .epp-inst.done .prerun.done
 	@echo EPP-RUN
 	cd $(FUNCTION) && \
 	export LD_LIBRARY_PATH=$(NEEDLE_LIB):/usr/local/lib64 && \
 	./$(NAME)-epp $(RUNCMD) 2>&1 > ../epp-run.log
-	@touch .epp-run.success
+	@touch .epp-run.done
 
-epp-decode: .epp-run.success .epp-decode.success
-.epp-decode.success: .epp-run.success 
+epp-decode: .epp-run.done .epp-decode.done
+.epp-decode.done: .epp-run.done 
 	@echo EPP-DECODE
 	cd $(FUNCTION) && \
 	export PATH=$(LLVM_OBJ):$(PATH) && \
     $(NEEDLE_OBJ)/epp -epp-fn=$(FUNCTION) $(NAME).bc -p=path-profile-results.txt 2> ../epp-decode.log
-	@touch .epp-decode.success
+	@touch .epp-decode.done
 
-needle-path: .epp-decode.success .needle-path.success
-.needle-path.success: .epp-decode.success 
+needle-path: .epp-decode.done .needle-path.done
+.needle-path.done: .epp-decode.done 
 	@echo NEEDLE-PATH
 	cd $(FUNCTION) && \
 	export PATH=$(LLVM_OBJ):$(PATH) && \
     python $(ROOT)/examples/scripts/path.py epp-sequences.txt > paths.stats.txt && \
 	$(NEEDLE_OBJ)/needle -fn=$(FUNCTION) -ExtractType::path -seq=path-seq-0.txt $(LIBS) -u=$(HELPER_LIB) $(NAME).bc -o $(NAME)-needle-0 2>&1 > ../needle-path.log
 
-needle-braid: .epp-decode.success .needle-braid.success
-.needle-braid.success: .epp-decode.success 
+needle-braid: .epp-decode.done .needle-braid.done
+.needle-braid.done: .epp-decode.done 
 	@echo NEEDLE-BRAID
 	cd $(FUNCTION) && \
 	export PATH=$(LLVM_OBJ):$(PATH) && \
     python $(ROOT)/examples/scripts/braid.py epp-sequences.txt > braids.stats.txt && \
 	$(NEEDLE_OBJ)/needle -fn=$(FUNCTION) -ExtractType::braid -seq=braid-seq-0.txt $(LIBS) -u=$(HELPER_LIB) $(NAME).bc -o $(NAME)-needle-0 2>&1 > ../needle-braid.log
 
-needle-run-braid: needle-braid prerun .prerun.success
+needle-run-braid: needle-braid prerun .prerun.done
+	@echo NEEDLE-RUN-BRAID
 	cd $(FUNCTION) && \
 	export LD_LIBRARY_PATH=$(NEEDLE_LIB):/usr/local/lib64 && \
 	./$(NAME)-needle-0 $(RUNCMD) 2>&1 > ../needle-run-braid.log
 
-needle-run-path: needle-path prerun .prerun.success
+needle-run-path: needle-path prerun .prerun.done
+	@echo NEEDLE-RUN-PATH
 	cd $(FUNCTION) && \
 	export LD_LIBRARY_PATH=$(NEEDLE_LIB):/usr/local/lib64 && \
 	./$(NAME)-needle-0 $(RUNCMD) 2>&1 > ../needle-run-path.log
 
 clean:
-	@rm -rf $(FUNCTION) *.log .*.success
+	@rm -rf $(FUNCTION) *.log .*.done
 
 .PHONY: clean setup
